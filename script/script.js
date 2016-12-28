f7fc736289531ed766e0bec47a9535fa029b8c8e
Olga Chavez <olgakatrin827@gmail.com>
2016-12-28 06:57:30 +0000

    nav bar working and gradient in place

--- script/script.js
+++ script/script.js
@@ -1,30 +1,58 @@
-var  mn = $(".main-nav");
-    mns = "main-nav-scrolled";
-    hdr = $('header').height();
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-$(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(mns);
-  } else {
-    mn.removeClass(mns);
-  }
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
 
-function myFunction(x) {
-    x.classList.toggle("change");
-}
 
+/*var mn = $(".main-nav");
+mns = "main-nav-scrolled";
+hdr = $('header').height();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > hdr) {
+    mn.addClass(mns);
+  }
+  else {
+    mn.removeClass(mns);
+  }
+});*/
+
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+/*
 //this is where we apply opacity to the arrow
-$(window).scroll( function(){
+$(window).scroll(function() {
 
   //get scroll position
   var topWindow = $(window).scrollTop();
   //multiply by 1.5 so the arrow will become transparent half-way up the page
   var topWindow = topWindow * 1.5;
-  
+
   //get height of window
   var windowHeight = $(window).height();
-      
+
   //set position as percentage of how far the user has scrolled 
   var position = topWindow / windowHeight;
   //invert the percentage
@@ -35,19 +63,16 @@ $(window).scroll( function(){
   $('.arrow-wrap').css('opacity', position);
 
 });
-
-
-
-
-
-
+*/
 
 //Code stolen from css-tricks for smooth scrolling:
+
+/*
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
@@ -57,3 +82,5 @@ $(function() {
     }
   });
 });
+
+*/
